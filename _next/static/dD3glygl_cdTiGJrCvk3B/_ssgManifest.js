7229389653d42244936d62140d95b52248ee63c3
ttesttes93405 <ttesttes93405@users.noreply.github.com>
2022-02-14 16:56:16 +0000

    deploy: 187ece113a4f19bd5418b765f858ef32468ae015

---
+++ _next/static/dD3glygl_cdTiGJrCvk3B/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F25sprout-portfolio","\u002F","\u002F25sprout\u002F[id]","\u002Fproject\u002F[id]","\u002Ftype\u002F[type]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
