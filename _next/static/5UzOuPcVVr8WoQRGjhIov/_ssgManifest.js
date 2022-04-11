66cecc3df260fde25a7d4485b36ca12cb35036bc
ttesttes93405 <ttesttes93405@users.noreply.github.com>
2022-04-11 15:17:39 +0000

    deploy: f3ec8845b461ace02f143c27fa922fcef0a4c763

---
+++ _next/static/5UzOuPcVVr8WoQRGjhIov/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fnsdi-portfolio","\u002Ftype\u002F[type]","\u002Fnsdi\u002F[id]","\u002Fproject\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
